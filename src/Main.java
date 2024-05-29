f88b53a735bd1c51ec52c26f96d203897dd37b20
Karla Brothánková <carly.b@seznam.cz>
2024-05-29 18:06:53 +0200

    Vytvoření proměnných
    Vytvoření proměnných pro uložení dat a vypsání průměrného množství prodané mrkve na 1 smlouvu.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String nameSurname = "Jan Novák";
+        LocalDate dateOfBirth = LocalDate.of(1963, 11, 8);
+        int numberOfContracts = 50;
+        double amountOfCarrot = 3.5;
+        String addressOfSalesman = "Praha";
+        String carRegistrationNumber = "5P1 88 66";
+        double fuelConsumption = 6.0;
+        String addressIPv4 = "192.168.0.0/16";
+
+        double averageAmountOfCarrot = amountOfCarrot / numberOfContracts;
+
+        System.out.println("Průměrné množství prodané mrkve na 1 smlouvu je: " + averageAmountOfCarrot + " tun.");
+
     }
 }
